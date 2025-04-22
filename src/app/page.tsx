36bdde9137e0dc4393d2e88bb9aea7feffbfb7a1
Andrej Jovanovski <andrejjovanovski17@gmail.com>
2025-04-22 13:52:20 +0200

    add base url for alias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
-import {AboutSection} from "@/sections/About";
-import {ContactSection} from "@/sections/Contact";
-import {Footer} from "@/sections/Footer"
+import { AboutSection}  from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer"
 
 export default function Home() {
   return (
